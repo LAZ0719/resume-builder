afc2483d9c7a17f5be4bff1896f99427ccfef978
lhayann <178252313+LAZ0719@users.noreply.github.com.>
2025-10-23 19:07:21 +0800

    feat: modify Layout.jsx

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
     <div>
-        <h1>Layout</h1>
+        <h1>Layout Page</h1>
+        <div>
+          <Outlet/>
+
+        </div>
     </div>
   )
 }
